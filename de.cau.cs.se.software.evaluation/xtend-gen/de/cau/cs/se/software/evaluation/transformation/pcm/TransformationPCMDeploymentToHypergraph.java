2fcf55ff1129dd84e7e1d7d890baaeb37735c75f
Reiner Jung <reiner.jung@email.uni-kiel.de>
2016-08-12 21:25:46 +0200

    minor modifications

--- de.cau.cs.se.software.evaluation/xtend-gen/de/cau/cs/se/software/evaluation/transformation/pcm/TransformationPCMDeploymentToHypergraph.java
+++ de.cau.cs.se.software.evaluation/xtend-gen/de/cau/cs/se/software/evaluation/transformation/pcm/TransformationPCMDeploymentToHypergraph.java
@@ -1,123 +1,78 @@
 package de.cau.cs.se.software.evaluation.transformation.pcm;
 
-import de.cau.cs.se.software.evaluation.hypergraph.HypergraphFactory;
-import de.cau.cs.se.software.evaluation.hypergraph.ModelElementTrace;
 import de.cau.cs.se.software.evaluation.hypergraph.ModularHypergraph;
 import de.cau.cs.se.software.evaluation.hypergraph.Module;
-import de.cau.cs.se.software.evaluation.hypergraph.ModuleReference;
 import de.cau.cs.se.software.evaluation.transformation.AbstractTransformation;
-import de.cau.cs.se.software.evaluation.transformation.HypergraphCreationHelper;
 import java.util.Arrays;
-import java.util.function.Consumer;
 import org.eclipse.core.runtime.IProgressMonitor;
-import org.eclipse.emf.common.util.EList;
-import org.eclipse.emf.ecore.EObject;
-import org.palladiosimulator.pcm.core.composition.AssemblyContext;
-import org.palladiosimulator.pcm.repository.BasicComponent;
-import org.palladiosimulator.pcm.repository.CompleteComponentType;
-import org.palladiosimulator.pcm.repository.CompositeComponent;
-import org.palladiosimulator.pcm.repository.OperationRequiredRole;
-import org.palladiosimulator.pcm.repository.ProvidedRole;
-import org.palladiosimulator.pcm.repository.RepositoryComponent;
-import org.palladiosimulator.pcm.repository.RequiredRole;
 
 @SuppressWarnings("all")
-public class TransformationPCMDeploymentToHypergraph extends AbstractTransformation<org.palladiosimulator.pcm.system.System, ModularHypergraph> {
+public class TransformationPCMDeploymentToHypergraph extends AbstractTransformation<System, ModularHypergraph> {
   public TransformationPCMDeploymentToHypergraph(final IProgressMonitor monitor) {
     super(monitor);
   }
   
   @Override
-  public ModularHypergraph generate(final org.palladiosimulator.pcm.system.System input) {
-    ModularHypergraph _createModularHypergraph = HypergraphFactory.eINSTANCE.createModularHypergraph();
-    this.result = _createModularHypergraph;
-    EList<AssemblyContext> _assemblyContexts__ComposedStructure = input.getAssemblyContexts__ComposedStructure();
-    final Consumer<AssemblyContext> _function = (AssemblyContext assemblyContext) -> {
-      String _entityName = assemblyContext.getEntityName();
-      String _plus = ("Module " + _entityName);
-      System.out.println(_plus);
-      String _entityName_1 = assemblyContext.getEntityName();
-      final Module module = HypergraphCreationHelper.createModule(this.result, _entityName_1, assemblyContext);
-      RepositoryComponent _encapsulatedComponent__AssemblyContext = assemblyContext.getEncapsulatedComponent__AssemblyContext();
-      this.createNodeForComponent(_encapsulatedComponent__AssemblyContext, module);
-    };
-    _assemblyContexts__ComposedStructure.forEach(_function);
-    EList<Module> _modules = this.result.getModules();
-    final Consumer<Module> _function_1 = (Module module) -> {
-      ModuleReference _derivedFrom = module.getDerivedFrom();
-      EObject _element = ((ModelElementTrace) _derivedFrom).getElement();
-      final RepositoryComponent component = ((AssemblyContext) _element).getEncapsulatedComponent__AssemblyContext();
-      if ((component instanceof BasicComponent)) {
-        String _entityName = ((BasicComponent) component).getEntityName();
-        String _plus = (">> " + _entityName);
-        System.out.println(_plus);
-        EList<RequiredRole> _requiredRoles_InterfaceRequiringEntity = ((BasicComponent)component).getRequiredRoles_InterfaceRequiringEntity();
-        final Consumer<RequiredRole> _function_2 = (RequiredRole required) -> {
-          boolean _matched = false;
-          if (required instanceof OperationRequiredRole) {
-            _matched=true;
-            ((OperationRequiredRole)required).getRequiredInterface__OperationRequiredRole();
-          }
-          if (!_matched) {
-            System.out.println(("other role " + required));
-          }
-        };
-        _requiredRoles_InterfaceRequiringEntity.forEach(_function_2);
-      }
-    };
-    _modules.forEach(_function_1);
-    return this.result;
+  public ModularHypergraph generate(final System input) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nAssemblyContext cannot be resolved to a type."
+      + "\nBasicComponent cannot be resolved to a type."
+      + "\nBasicComponent cannot be resolved to a type."
+      + "\nOperationRequiredRole cannot be resolved to a type."
+      + "\nThe method or field assemblyContexts__ComposedStructure is undefined for the type System"
+      + "\nThe method or field entityName is undefined for the type EObject"
+      + "\nThe method or field entityName is undefined for the type EObject"
+      + "\nThe method or field encapsulatedComponent__AssemblyContext is undefined for the type EObject"
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or put the closures into a typed context."
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or put the closures into a typed context."
+      + "\nforEach cannot be resolved"
+      + "\ncreateNodeForComponent cannot be resolved"
+      + "\nencapsulatedComponent__AssemblyContext cannot be resolved"
+      + "\nentityName cannot be resolved"
+      + "\ngetRequiredRoles_InterfaceRequiringEntity cannot be resolved"
+      + "\nforEach cannot be resolved"
+      + "\nrequiredInterface__OperationRequiredRole cannot be resolved");
   }
   
-  private void _createNodeForComponent(final BasicComponent component, final Module module) {
-    EList<ProvidedRole> _providedRoles_InterfaceProvidingEntity = component.getProvidedRoles_InterfaceProvidingEntity();
-    final Consumer<ProvidedRole> _function = (ProvidedRole provided) -> {
-      String _entityName = provided.getEntityName();
-      String _plus = ("node " + _entityName);
-      System.out.println(_plus);
-      String _entityName_1 = provided.getEntityName();
-      HypergraphCreationHelper.createNode(this.result, module, _entityName_1, provided);
-    };
-    _providedRoles_InterfaceProvidingEntity.forEach(_function);
+  private void _createNodeForComponent(final /* BasicComponent */Object component, final Module module) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method or field entityName is undefined for the type EObject"
+      + "\nThe method or field entityName is undefined for the type EObject"
+      + "\nThere is no context to infer the closure\'s argument types from. Consider typing the arguments or put the closures into a typed context."
+      + "\nprovidedRoles_InterfaceProvidingEntity cannot be resolved"
+      + "\nforEach cannot be resolved");
   }
   
-  private void _createNodeForComponent(final CompleteComponentType component, final Module module) {
-    String _entityName = component.getEntityName();
-    HypergraphCreationHelper.createNode(this.result, module, _entityName, component);
+  private void _createNodeForComponent(final /* CompleteComponentType */Object component, final Module module) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nentityName cannot be resolved");
   }
   
-  private void _createNodeForComponent(final CompositeComponent component, final Module module) {
-    String _entityName = component.getEntityName();
-    HypergraphCreationHelper.createNode(this.result, module, _entityName, component);
+  private void _createNodeForComponent(final /* CompositeComponent */Object component, final Module module) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nentityName cannot be resolved");
   }
   
-  private void _createNodeForComponent(final RepositoryComponent component, final Module module) {
-    System.out.println(("strange component type " + component));
+  private void _createNodeForComponent(final /* RepositoryComponent */Object component, final Module module) {
+    String _plus = ("strange component type " + component);
+    System.out.println(_plus);
   }
   
   @Override
-  public int workEstimate(final org.palladiosimulator.pcm.system.System input) {
-    EList<AssemblyContext> _assemblyContexts__ComposedStructure = input.getAssemblyContexts__ComposedStructure();
-    int _size = _assemblyContexts__ComposedStructure.size();
-    return (_size * 2);
+  public int workEstimate(final System input) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nThe method or field assemblyContexts__ComposedStructure is undefined for the type System"
+      + "\nsize cannot be resolved"
+      + "\n* cannot be resolved");
   }
   
-  private void createNodeForComponent(final RepositoryComponent component, final Module module) {
-    if (component instanceof BasicComponent) {
-      _createNodeForComponent((BasicComponent)component, module);
-      return;
-    } else if (component instanceof CompositeComponent) {
-      _createNodeForComponent((CompositeComponent)component, module);
-      return;
-    } else if (component instanceof CompleteComponentType) {
-      _createNodeForComponent((CompleteComponentType)component, module);
-      return;
-    } else if (component != null) {
+  private void createNodeForComponent(final BasicComponent component, final Module module) {
+    if (component != null) {
       _createNodeForComponent(component, module);
-      return;
-    } else {
-      throw new IllegalArgumentException("Unhandled parameter types: " +
-        Arrays.<Object>asList(component, module).toString());
+      return; else {
+        throw new IllegalArgumentException("Unhandled parameter types: " +
+          Arrays.<Object>asList(component, module).toString());
+      }
     }
   }
-}
+  
