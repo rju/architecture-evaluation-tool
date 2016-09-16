7bde1ac43c957f4fec9a0e7be6723576147b7bbb
Reiner Jung <reiner.jung@email.uni-kiel.de>
2016-09-17 00:26:19 +0200

    minor modification

--- de.cau.cs.se.software.evaluation/src/de/cau/cs/se/software/evaluation/commands/AbstractAnalysisHandler.java
+++ de.cau.cs.se.software.evaluation/src/de/cau/cs/se/software/evaluation/commands/AbstractAnalysisHandler.java
@@ -1,3 +1,18 @@
+/***************************************************************************
+ * Copyright 2015
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
 package de.cau.cs.se.software.evaluation.commands;
 
 import org.eclipse.core.commands.AbstractHandler;
@@ -28,13 +43,27 @@ public abstract class AbstractAnalysisHandler extends AbstractHandler implements
 		return null;
 	}
 
-	abstract protected void executeCalculation(ISelection selection, IWorkbenchPage activePage, Shell shell) throws ExecutionException;
+	/**
+	 * Calculate the complete execution time of a job.
+	 *
+	 * @param selection
+	 *            the selection which is processed by the job
+	 * @param activePage
+	 *            the active workbench page
+	 * @param shell
+	 *            the display shell
+	 * @throws ExecutionException
+	 *             thrown on any eclipse error
+	 */
+	protected abstract void executeCalculation(ISelection selection, IWorkbenchPage activePage, Shell shell) throws ExecutionException;
 
 	/**
 	 * Start analysis view.
 	 *
 	 * @param activePage
+	 *            the active workbench page
 	 * @throws ExecutionException
+	 *             thrown on any eclipse error
 	 */
 	protected void createAnalysisView(final IWorkbenchPage activePage) throws ExecutionException {
 		try {
@@ -50,7 +79,9 @@ public abstract class AbstractAnalysisHandler extends AbstractHandler implements
 	 * Start log view.
 	 *
 	 * @param activePage
+	 *            the active workbench page
 	 * @throws ExecutionException
+	 *             thrown on any eclipse error
 	 */
 	protected void createLogView(final IWorkbenchPage activePage) throws ExecutionException {
 		try {
